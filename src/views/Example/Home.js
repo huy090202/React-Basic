091edcd2d3012cb6e6b2defaa8d081c051dee392
huy090202 <pham.dang.huy092@gmail.com>
2023-10-26 23:06:56 +0700

    26/10/2023

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { withRouter } from "react-router";
+import Color from "../HOC/Color";
 
 class Home extends React.Component {
+  componentDidMount() {
+    // setTimeout(() => {
+    //   // Chuyen sang trang todo
+    //   // Co thuoc tinh history.push ,... la nho HOC
+    //   this.props.history.push("/todo");
+    // }, 3000);
+  }
+
   render() {
     return <div>Hello world from HomePage with Huy</div>;
   }
 }
 
-export default Home;
+// HOC(higher order component): khi withRouter boc thang home => tao ra component moi co tat ca thuoc tinh cua withRouter + home
+// export default withRouter(Home);
+export default Color(Home);
