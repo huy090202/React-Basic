b806ae403c741299984ab471dcc78a18adecf223
huy090202 <pham.dang.huy092@gmail.com>
2023-10-28 22:04:13 +0700

    28/10/2023(update)

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -2,12 +2,34 @@ const initState = {
   users: [
     { id: 1, name: "Huy" },
     { id: 2, name: "Dang Huy" },
+    { id: 3, name: "Pham Dang Huy" },
   ],
+
+  posts: [],
 };
 
 // state la trang thai cua redux, noi luu tru data cua redux
 const rootreducer = (state = initState, action) => {
-  return state;
+  switch (action.type) {
+    case "DELETE_USER":
+      let users = state.users;
+      users = users.filter((item) => item.id !== action.payload.id);
+
+      //De app k bi anh huong khi co nhieu du lieu thi can copy lai state, users se lay cai moi hon va ghi de lai
+      return {
+        ...state,
+        users,
+      };
+    case "CREATE_USER":
+      let id = Math.floor(Math.random() * 10000);
+      let user = { id: id, name: `random - ${id}` };
+      return {
+        ...state,
+        users: [...state.users, user],
+      };
+    default:
+      return state;
+  }
 };
 
 export default rootreducer;
--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -3,6 +3,7 @@ import { withRouter } from "react-router";
 import Color from "../HOC/Color";
 import Images from "../../assets/images/PHẠM ĐANG HUY.png";
 import "./demo.scss";
+import { connect } from "react-redux";
 
 class Home extends React.Component {
   componentDidMount() {
@@ -13,18 +14,54 @@ class Home extends React.Component {
     // }, 3000);
   }
 
+  handleDeleteUser = (user) => {
+    this.props.deleteUserRedux(user);
+  };
+
+  handleCreateUser = () => {
+    this.props.addUserRedux();
+  };
+
   render() {
+    let listUsers = this.props.dataRedux;
     return (
       <>
         <div>Hello world from HomePage with Huy</div>
         <div>
           <img className="img-test" src={Images} />
         </div>
+        <div>
+          {listUsers &&
+            listUsers.length > 0 &&
+            listUsers.map((item, index) => {
+              return (
+                <div key={item.id}>
+                  {index + 1} - {item.name} &nbsp;
+                  <span onClick={() => this.handleDeleteUser(item)}>[x]</span>
+                </div>
+              );
+            })}
+          <button onClick={() => this.handleCreateUser()}>Add</button>
+        </div>
       </>
     );
   }
 }
 
+const mapStateToProps = (state) => {
+  return {
+    dataRedux: state.users,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    deleteUserRedux: (userDelete) =>
+      dispatch({ type: "DELETE_USER", payload: userDelete }),
+    addUserRedux: () => dispatch({ type: "CREATE_USER" }),
+  };
+};
+
 // HOC(higher order component): khi withRouter boc thang home => tao ra component moi co tat ca thuoc tinh cua withRouter + home
 // export default withRouter(Home);
-export default Color(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Color(Home));
